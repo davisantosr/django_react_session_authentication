ae654663aadadc282767ffc4d2a635806fa7bc32
davisantosr <davi-rocha2@hotmail.com>
2020-12-15 14:08:01 -0300

    add get and update userprofile|urls

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
-    fields = '__all__'
-    
+    fields = ('id', 'name')
+
 
 
--- backend/user_profile/urls.py
+++ backend/user_profile/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
+from .views import GetUserProfileView, UpdateUserProfileView
 
 urlpatterns = [
-  
+  path('user/', GetUserProfileView.as_view()),
+  path('update/', UpdateUserProfileView.as_view()),
 ]
