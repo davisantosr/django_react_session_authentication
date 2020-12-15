44801cc1409c6a74683cb15263024408869ccded
davisantosr <davi-rocha2@hotmail.com>
2020-12-15 10:56:02 -0300

    add class GetCSRFToken

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework import permissions
 from rest_framework.response import Response
+from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
 
 from user_profile.models import UserProfile
@@ -38,3 +39,11 @@ class SignUpView(APIView):
 
     else: 
       return Response({"error": "passwords do not match"})
+
+
+@method_decorator(ensure_csrf_cookie, name='dispatch')
+class GetCSRFToken(APIView):
+  permission_classes = (permissions.AllowAny,)
+
+  def get(self, request, format=None):
+    return Response({'success': 'CSRF cookie set'})
