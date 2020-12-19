e6955b70343d2ba738c1afd488c2f99e2072de0d
davisantosr <davi-rocha2@hotmail.com>
2020-12-19 20:08:31 -0300

    add files for components and containers|add bootstrap cdn|

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+import Layout from './hocs/Layout'
+
+import Home from './containers/home/Home'
+import Register from './containers/register/Register'
+import Dashboard from './containers/home/Home'
+import Login from './containers/login/Login'
+
 const App = () => (
   <Router>
-    
+    <Layout>
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/register' component={Register}/>
+      <Route exact path='/login' component={Login}/>
+      <Route exact path='/dashboard' component={Dashboard}/>
+
+    </Layout>
   </Router>
 )
+
+export default App
