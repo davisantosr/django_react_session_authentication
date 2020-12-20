5cfe82aba81d1a35cbcdc0fea45224c44df7b266
davisantosr <davi-rocha2@hotmail.com>
2020-12-19 22:47:05 -0300

    add register page functionality|config package.json for build

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 import {REGISTER_SUCCESS, REGISTER_FAIL } from './types'
 
-export const register = (username, password, re_password) = async dispatch => {
+export const register = (username, password, re_password) => async dispatch => {
   const config = {
     headers: {
       'Accept': 'application/json',
--- frontend/src/containers/register/Register.js
+++ frontend/src/containers/register/Register.js
@@ -1,9 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import { Link, Redirect } from 'react-router-dom';
+
+import { register } from '../../actions/auth'
+
+const Register = ({register}) => {
+  const [ formData, setFormData ] = useState({
+    username: '', 
+    password: '', 
+    re_password: '',
+  });
+
+  const [ accountCreated, setAccountCreated ] = useState(false)
+
+  const { username, password, re_password } = formData;
+
+  const onChange = event => setFormData({...formData, [event.target.name]: event.target.value});
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    if(password === re_password) {
+      register(username, password, re_password);
+      setAccountCreated(true);
+
+    }
+  };
+
+  if(accountCreated) {
+    return <Redirect to='/' />
+  }
+
+  return (
+    <div className='container mt-5'>
+      <h1>Register an account</h1>
+      <p>Crate an account with our Session Auth application</p>
+      <form onSubmit={e => onSubmit(e)}>
+        <div className='form-group'>
+          <label className='form-label'>Username</label>
+          <input
+            className='form-control'
+            type='text'
+            placeholder='username *'
+            name='username'
+            onChange={e => onChange(e)}
+            value={username}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <label className='form-label mt-3'>Password</label>
+          <input
+            className='form-control'
+            type='password'
+            placeholder='Password'
+            name='password'
+            onChange={e => onChange(e)}
+            value={password}
+            minLength='6'
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <label className='form-label mt-3'>Confirm Password</label>
+          <input
+            className='form-control'
+            type='password'
+            placeholder='Confirm Password'
+            name='re_password'
+            onChange={e => onChange(e)}
+            value={re_password}
+            minLength='6'
+            required
+          />
+        </div>
+        <button className='btn btn-primary mt-3' type='submit'>Register</button>
+      </form>
+      <p className='mt-3'>
+        Already have an account? <Link to='/login'>Sign In</Link>
+      </p>
 
 
-const Register = () => (
+    </div>
+  )
 
-  <div> Register </div>
-)
+}
 
-export default Register
+export default connect(null, {register})(Register)
