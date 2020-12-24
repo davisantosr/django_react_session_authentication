234b82a3d08daec9bacffd1d7de0a73c31384c2b
davisantosr <davi-rocha2@hotmail.com>
2020-12-23 22:40:34 -0300

    add login logout reducers

--- frontend/src/reducers/auth.js
+++ frontend/src/reducers/auth.js
@@ -1,7 +1,11 @@
 import { act } from 'react-dom/test-utils';
 import {
   REGISTER_SUCCESS,
-  REGISTER_FAIL
+  REGISTER_FAIL,
+  LOGIN_SUCCESS, 
+  LOGIN_FAIL, 
+  LOGOUT_SUCCESS,
+  LOGOUT_FAIL,
  } from '../actions/types';
 
 
@@ -23,7 +27,22 @@ import {
         ...state,
         isAuthenticated: false
       }
+    case LOGIN_SUCCESS:
+      return {
+        ...state, 
+        isAuthenticated: true,
+        username: payload
+      }
+    case LOGOUT_SUCCESS:
+      return {
+        ...state,
+        isAuthenticated: false, 
+        username: ''
+      }
+    
     case REGISTER_FAIL:
+    case LOGIN_FAIL:
+    case LOGOUT_FAIL:
       return state
 
     default:
